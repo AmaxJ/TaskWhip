2e37fb178a0360c4f31834ab23f045d076107501
alan johnson <ajohnson88@gmail.com>
2015-08-15 11:08:58 -0400

    Refactored task test cases. Removed flask-testing.

--- tests/test_taskdb.py
+++ tests/test_taskdb.py
@@ -3,57 +3,48 @@ import os
 sys.path.insert(0, os.path.abspath('..'))
 import unittest
 from datetime import datetime
-from flask.ext.testing import TestCase
+from basetest import DatabaseTestCase
 from api import app, db
 from api.models.users import User
 from api.models.tasks import Task
-from api.models.groups import Group 
+from api.models.groups import Group
 
 
-class TaskDBTests(TestCase):
-    
-    def create_app(self):    
-        app.config.from_object('config.TestConfig')
-        return app
+class TasksDBTestCase(DatabaseTestCase):
+
+    def __init__(self, *args, **kwargs):
+        DatabaseTestCase.__init__(self,*args, **kwargs)
 
     def setUp(self):
         db.create_all()
-        user = User(username="Oscar",
-                    email="acosta@yahoo.com")
         group = Group(name="Test group",
                       created=datetime.utcnow(),
                       description="test 1 2 3")
-        db.session.add(group)
-        db.session.add(user)
-        db.session.commit()
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def test_task_was_created(self):
-        """Test task created and stored in db with
-        specified parameters."""
-        #no tasks:
-        self.assertEqual(len(Task.query.all()), 0)
         task = Task(title="Go to store",
                     body="Get eggs and milk.",
                     group_id=1, createdOn=datetime.utcnow())
+        db.session.add(group)
         db.session.add(task)
         db.session.commit()
-        task = Task.query.filter_by(id=1).first();
+
+    def test_task_was_created_with_params(self):
+        """Test task created and stored in db with the
+        specified parameters."""
+        #one task in db currently
         self.assertEqual(len(Task.query.all()), 1)
-        self.assertEqual(task.title, 'Go to store')
-        self.assertEqual(task.body, 'Get eggs and milk.')
+        task = Task(title="Go to beach",
+                    body="Get a tan.",
+                    group_id=1, createdOn=datetime.utcnow())
+        db.session.add(task)
+        db.session.commit()
+        task = Task.query.filter_by(id=2).first();
+        self.assertEqual(len(Task.query.all()), 2)
+        self.assertEqual(task.title, 'Go to beach')
+        self.assertEqual(task.body, 'Get a tan.')
         self.assertEqual(task.group_id, 1)
 
     def test_task_toggleComplete(self):
         """Test toggleCommplete() method"""
-        task = Task(title="Go to store",
-                    body="Get eggs and milk.",
-                    group_id=1, createdOn=datetime.utcnow())
-        db.session.add(task)
-        db.session.commit()
         task = Task.query.filter_by(id=1).first()
         self.assertEqual(task.complete, False)
         task.toggleComplete()
@@ -61,6 +52,18 @@ class TaskDBTests(TestCase):
         task.toggleComplete(False)
         self.assertEqual(task.complete, False)
 
+    def test_task_createdOn_is_datetime(self):
+        """Tests createdOn attribute is datetime object"""
+        task = Task.query.filter_by(id=1).first()
+        self.assertIsInstance(task.createdOn, datetime)
 
-if __name__=='__main__':
+    def test_task_completedOn_is_datetime(self):
+        """Tests completedOn attribute is datetime object"""
+        task = Task.query.filter_by(id=1).first()
+        task.toggleComplete()
+        self.assertIsInstance(task.completedOn, datetime)
+
+
+
+if __name__ == '__main__':
     unittest.main()
