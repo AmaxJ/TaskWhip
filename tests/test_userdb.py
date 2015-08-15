a9bd0c0221e13e46e69088fd61070a29327920dc
alan johnson <ajohnson88@gmail.com>
2015-08-15 11:23:28 -0400

    refactored userdb tests. Removed flask-testing

--- tests/test_userdb.py
+++ tests/test_userdb.py
@@ -2,49 +2,57 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
 import unittest
-from flask.ext.testing import TestCase
+from basetest import DatabaseTestCase
 from api import app, db
 from api.models.users import User
 from api.models.groups import Company
 
+class UserDBTests(DatabaseTestCase):
 
-class UserDBTests(TestCase):
-
-    def create_app(self):
-        app.config.from_object('config.TestConfig')
-        return app
+    def __init__(self, *args, **kwargs):
+        DatabaseTestCase.__init__(self,*args, **kwargs)
 
     def setUp(self):
         db.create_all()
-        company = Company(name="Test Company")
-
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-
-    def test_create_user(self):
-        """Test user is successfuly created and stored
-        in the database"""
-        #test no users first:
-        self.assertEqual(len(User.query.all()), 0)
+        company = Company(name="test company")
         user = User(username='guido',
                     email='vanRossum@gmail.com',
                     rank='Admin', company_id=1)
+        db.session.add(company)
         db.session.add(user)
         db.session.commit()
+
+    def test_user_is_created_in_db(self):
+        """Test user is successfuly created and stored
+        in the database"""
+        #One user in db currently
         self.assertEqual(len(User.query.all()), 1)
-        self.assertEqual(user.email, 'vanRossum@gmail.com')
-        self.assertEqual(user.username, 'guido')
+        user = User(username='patrice',
+                    email='oneal@gmail.com',
+                    rank='Admin', company_id=1)
+        db.session.add(user)
+        db.session.commit()
+        self.assertEqual(len(User.query.all()), 2)
+        self.assertEqual(user.email, 'oneal@gmail.com')
+        self.assertEqual(user.username, 'patrice')
         self.assertEqual(user.rank, 'Admin')
         self.assertEqual(user.company_id, 1)
-        
+
+    def test_user_is_deleted_from_db(self):
+        """Test user is successfully deleted from the database"""
+        #one user in db currently
+        self.assertEqual(len(User.query.all()), 1)
+        user = User.query.filter_by(username="guido").first()
+        db.session.delete(user)
+        db.session.commit()
+        self.assertEqual(len(User.query.all()), 0)
+
+
     def test_password_verification(self):
         """Test user's password is registered and
         successfully verified"""
-        user = User(username='Shane',
-                    email='Vendrell@gmail.com')
+        user = User.query.filter_by(username="guido").first()
         user.hash_password('python')
-        db.session.add(user)
         db.session.commit()
         self.assertEqual(user.verify_password('python'), True)
 
