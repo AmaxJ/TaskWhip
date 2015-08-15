d44c7673ded3e18f981303979801340dab6fd26f
alan johnson <ajohnson88@gmail.com>
2015-08-15 01:51:16 -0400

    added _parse_ids() method to GroupAPI, refactored put method.

--- api/resources/groups.py
+++ api/resources/groups.py
@@ -18,7 +18,7 @@ group_fields = {
 
 
 class GroupList(Resource):
-    """Returns a list of all groups for all companies"""
+    """Resource for all groups for all companies"""
 
     def get(self):
         groups = Group.query.all()
@@ -33,7 +33,7 @@ class GroupList(Resource):
 
 
 class GroupListByCompanyId(Resource):
-    """Returns a list of all groups for a specific company"""
+    """Resource for all groups of a specific company by ID"""
 
     def __init__(self):
         self.parser = reqparse.RequestParser(bundle_errors=True)
@@ -53,8 +53,9 @@ class GroupListByCompanyId(Resource):
             try:
                 return {"groups":[marshal(group, group_fields) for group in groups] }, 200
             except Exception as e:
-                print "ERROR: ", e
-                return {"error":"Problem retrieving groups"}, 404
+                print e
+                return {"error":"Problem retrieving groups",
+                        "msg" : str(e)}, 404
         else:
             return {"error":"No groups found"}, 404
 
@@ -67,36 +68,45 @@ class GroupListByCompanyId(Resource):
                     company_id=company_id
             )
             if args["admins"] is not None:
+                #admins will be passed as a string of comma separated id numbers
                 admins = [int(id) for id in args["admins"].split(",")]
                 for id in admins:
                     admin = User.query.filter_by(id=id).first()
                     if admin is not None:
                         newGroup.admins.append(admin)
             if args["members"] is not None:
+                #members passed as a string of comma separated id numbers
                 members = [int(id) for id in args["members"].split(",")]
                 for id in members:
                     member = User.query.filter_by(id=id).first()
                     if member is not None:
-                        newGroup.members.append(member)                    # except Exception as e:
+                        newGroup.members.append(member)
             db.session.add(newGroup)
             db.session.commit()
             return {"group": marshal(newGroup, group_fields)}
         except Exception as e:
             print e
-            return {"error":"Problem creating group"}, 404
+            return {"error":"Problem creating group",
+                    "msg" : str(e) }
 
 
 class GroupAPI(Resource):
-    """Returns an individual group."""
+    """Resource for an individual group."""
 
     def __init__(self):
         self.parser = reqparse.RequestParser(bundle_errors=True)
         self.parser.add_argument("description", type=str, location="json",
                                  trim=True)
         #members and admins will be a string of id numbers separated by commas.
-        self.parser.add_argument("members", type=str, location="json")
-        self.parser.add_argument("admins", type=str, location="json")
+        self.parser.add_argument("add_members", type=str, location="json")
+        self.parser.add_argument("add_admins", type=str, location="json")
+        self.parser.add_argument("remove_members", type=str, location="json")
+        self.parser.add_argument("remove_admins", type=str, location="json")
+
         super(GroupAPI, self).__init__()
+
+    def _parse_ids(self, idString):
+        return map(lambda x: int(x), idString.split(","))
 
     def get(self, company_id, id):
         group = Group.query.filter_by(company_id=company_id, id=id).first()
@@ -105,42 +115,46 @@ class GroupAPI(Resource):
                 return { "group": marshal(group, group_fields) }
             except Exception as e:
                 print "Error: ", e
-                return { "error": "Error retrieving group" }
+                return { "error": "Error retrieving group",
+                         "msg" : str(e) }
         else:
-            return { "error" : "Group not found" }
+            return { "error" : "Group not found" }, 404
 
-    #TODO:
-        #make an add user/admin method and also make a way to remove users.
-        #clean this mess up
     def put(self, company_id, id):
         group = Group.query.filter_by(company_id=company_id, id=id).first()
         if group is not None:
             try:
                 args = self.parser.parse_args()
-                if args["members"] is not None:
-                    members = [int(memberID) for memberID in args["members"].split(",")]
-                    for userID in members:
-                        member = User.query.filter_by(id=userID).first()
-                        if member not in group.members:
-                            group.members.append(member)
-                    db.session.commit()
+                if args["add_members"] is not None:
+                    member_ids = self._parse_ids(args["add_members"])
+                    members = [User.query.filter_by(id=id).first() for id in member_ids]
+                    group.add_members(members)
                     return {"group" : marshal(group, group_fields) }, 200
-                elif args["admins"] is not None:
-                    admins = [int(adminID) for adminID in args["admins"].split(",")]
-                    for adminID in admins:
-                        admin = User.query.filter_by(id=adminID).first()
-                        if admin not in group.admins:
-                            group.admins.append(admin)
-                    db.session.commit()
+                elif args["remove_members"] is not None:
+                    member_ids = self._parse_ids(args["remove_members"])
+                    members = [User.query.filter_by(id=id).first() for id in member_ids]
+                    group.remove_members(members)
                     return {"group" : marshal(group, group_fields) }, 200
-                for key, value in args.items():
-                    if args[key] is not None and hasattr(group, key):
-                        setattr(group, key, value)
-                db.session.commit()
-                return {"group" : marshal(group, group_fields)}, 200
+                elif args["add_admins"] is not None:
+                    admin_ids = self._parse_ids(args["add_admins"])
+                    admins = [User.query.filter_by(id=id).first() for id in admin_ids]
+                    group.add_members(admins, admin=True)
+                    return {"group" : marshal(group, group_fields) }, 200
+                elif args["remove_admins"] is not None:
+                    admin_ids = self._parse_ids(args["remove_admins"])
+                    admins = [User.query.filter_by(id=id).first() for id in admin_ids]
+                    group.remove_members(admins, admin=True)
+                    return {"group" : marshal(group, group_fields) }, 200
+                else:
+                    for key, value in args.items():
+                        if args[key] is not None and hasattr(group, key):
+                            setattr(group, key, value)
+                    db.session.commit()
+                    return {"group" : marshal(group, group_fields)}, 200
             except Exception as e:
                 print e
-                return {"error":"Error editing group"}
+                return {"error":"Error editing group",
+                        "msg" : str(e) }
         else:
             return {"error":"Group not found"}
 
