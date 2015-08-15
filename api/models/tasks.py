09729fb460bc58fa476761154217d14c5fea5fae
alan johnson <ajohnson88@gmail.com>
2015-08-15 02:31:07 -0400

    updated doc for toggleComplete()

--- api/models/tasks.py
+++ api/models/tasks.py
@@ -5,7 +5,7 @@ from mixins import DbMixin
 
 
 class Task(db.Model, DbMixin):
-    __tablename__ = 'task'
+    # __tablename__ = 'task'
     id = db.Column(db.Integer, primary_key=True)
     createdOn = db.Column(db.DateTime, default=datetime.now)
     completedOn = db.Column(db.DateTime)
@@ -26,14 +26,25 @@ class Task(db.Model, DbMixin):
         return "{}: {}".format(self.group_id, self.title)
 
     def toggleComplete(self, done=True):
+        """Updates the appropriate attributes when a task's status has been
+        changed.
+
+        If 'done' flag is set to False, a completed task may be returned to
+        'pending' status. Upon task completion, the length of time the task was
+        'pending' is recorded and the timeActive attribute is set accordingly.
+        If the task is returned to incomplete status, and then back to complete,
+        the newly elapsed time will be recorded.
+        """
         if done:
             setattr(self, "complete", True)
             setattr(self, "completedOn", datetime.now())
             setattr(self, "status", "completed")
             delta = self.completedOn - self.createdOn
             setattr(self, "timeActive", delta.seconds)
+            db.session.commit()
         else:
             setattr(self, "complete", False)
             setattr(self, "status", "pending")
+            db.session.commit()
 
 
